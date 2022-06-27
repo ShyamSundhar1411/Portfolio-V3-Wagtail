199873102633aefe0d1725067e46fc493a3f4d72
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-06-27 18:17:48 +0530

    Reverting to PythonAnywhere

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -160,11 +160,12 @@ STATICFILES_FINDERS = [
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/4.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR,'static')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'portfolio/static'
-]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
