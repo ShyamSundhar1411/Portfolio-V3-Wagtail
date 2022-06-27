f89f3a17440795b306fae5d5da3f9e2f0415b97a
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-06-27 15:44:49 +0530

    Production Optimising

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.production")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
