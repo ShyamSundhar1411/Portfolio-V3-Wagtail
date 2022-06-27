a72152f81a40d53d7be3e15d9abb491ebfd433b9
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-06-27 16:45:37 +0530

    Production Optimising

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = env['SECRET_KEY']
 DEBUG = False
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 DATABASES['default'] = dj_database_url.config()
 DEFAULT_FILE_STORAGE = env['DEFAULT_FILE_STORAGE']
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO","https")
