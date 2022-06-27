08577e3737030300838a5c96927950dab61f9d43
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-06-27 09:23:02 +0530

    Production Optimising

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -7,13 +7,7 @@ SECRET_KEY = env['SECRET_KEY']
 DEBUG = True
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-COMPRESS_OFFLINE = True
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter'
-]
-COMPRESS_CSS_HASHING_METHOD = "content"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DATABASES['default'] = dj_database_url.config()
 DEFAULT_FILE_STORAGE = env['DEFAULT_FILE_STORAGE']
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO","https")
