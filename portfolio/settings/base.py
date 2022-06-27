e5c40b42213951478c52c3623a29c73bce3e2ada
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-06-27 16:02:53 +0530

    Production Optimising

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -162,9 +162,7 @@ STATICFILES_FINDERS = [
 # See https://docs.djangoproject.com/en/4.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
-]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
