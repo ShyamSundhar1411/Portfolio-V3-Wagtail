18d6d370b8d0f0ad7e74902a9a297cb43a2b2600
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-06-27 20:07:16 +0530

    Working

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -12,6 +12,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DATABASES['default'] =  dj_database_url.config()
 DEFAULT_FILE_STORAGE = env.str('DEFAULT_FILE_STORAGE')
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO","https")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_USER_SSL = False
+EMAIL_HOST = env.str('EMAIL_HOST',default = '')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER',default = '')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD',default = '')
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = "Notification Bot<noreply@shyamsundhar.com>"
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env.str('CLOUD_NAME'),
     'API_KEY': env.str('API_KEY'),
