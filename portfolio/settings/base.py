29b7c7b9626bc789fada3a2067fb6d0477e9c9b9
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-06-27 18:07:57 +0530

    Reverting to PythonAnywhere

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -162,7 +162,9 @@ STATICFILES_FINDERS = [
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'portfolio/static'
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
