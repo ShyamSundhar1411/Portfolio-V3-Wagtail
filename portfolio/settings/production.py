6173d5186c597d76b887f75eaf7e5027e2f91500
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-06-27 17:23:45 +0530

    Reverting to PythonAnywhere

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -1,18 +1,20 @@
 from __future__ import absolute_import,unicode_literals
 from .base import *
+import environ
 import dj_database_url
 import os
-env  = os.environ.copy()
-SECRET_KEY = env['SECRET_KEY']
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env.str['SECRET_KEY']
 DEBUG = False
 
 DATABASES['default'] = dj_database_url.config()
-DEFAULT_FILE_STORAGE = env['DEFAULT_FILE_STORAGE']
+DEFAULT_FILE_STORAGE = env.str['DEFAULT_FILE_STORAGE']
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO","https")
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env['CLOUD_NAME'],
-    'API_KEY': env['API_KEY'],
-    'API_SECRET': env['API_SECRET']
+    'CLOUD_NAME': env.str['CLOUD_NAME'],
+    'API_KEY': env.str['API_KEY'],
+    'API_SECRET': env.str['API_SECRET']
 }
 ALLOWED_HOSTS =["*"]
 try:
