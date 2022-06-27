643772ce3f86dc7461bf3fcbb9cd7a3d56eed52c
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-06-27 08:42:51 +0530

    Production Optimising

---
+++ portfolio/settings/production.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import,unicode_literals
+from .base import *
+import dj_database_url
+import os
+env  = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+DEBUG = False
+DATABASES['default'] = dj_database_url.config()
+DEFAULT_FILE_STORAGE = env['DEFAULT_FILE_STORAGE']
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO","https")
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env['CLOUD_NAME'],
+    'API_KEY': env['API_KEY'],
+    'API_SECRET': env['API_SECRET']
+}
+ALLOWED_HOSTS =["*"]
+try:
+    from .local import *
+except ImportError:
+    pass
